593e4dcec13382095a0c4f0b6a353bafd9b934ee
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-23 22:33:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,9 +19,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    words = params[:word1, :word2, :word3, :word4, :word5].to_s
+    words = params[:word1, :word2, :word3, :word4, :word5].to_a
     "#{words}"
-
   end
 
   get '/:operation/:number1/:number2' do
