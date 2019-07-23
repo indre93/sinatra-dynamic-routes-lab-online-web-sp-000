4bb79411dc93e6cf40ba60de075105805e027c25
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-23 22:27:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,7 @@ class App < Sinatra::Base
 
   get '/square/:number' do
     @number = params[:number].to_i
-    square_num = @number * @number
-    "#{square_num}"
+    "#{@number * @number}"
   end
 
   get '/say/:number/:phrase' do
